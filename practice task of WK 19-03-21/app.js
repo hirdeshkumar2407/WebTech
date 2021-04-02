4f40752251764c434fa4d5e79a96e51f91576402
Hirdesh Kumar <79218874+hirdeshkumar2407@users.noreply.github.com>
2021-04-03 03:44:59 +0500

    Update app.js
    Atlas Cloud connection successfully responding to the changes of CRUD operations

--- practice task of WK 19-03-21/app.js
+++ practice task of WK 19-03-21/app.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect("mongodb://localhost/studentsDb",{useNewUrlParser: true, useUnifiedTopology: true })
+//This connection responds to my altas cluster studentsDb
+mongoose.connect("mongodb+srv://Hirdesh:12345@cluster0.0rbdn.mongodb.net/studentsDb?retryWrites=true&w=majority",{useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=>{
     console.log('Connected to the Database');
 })
@@ -15,6 +16,16 @@ mongoose.connect("mongodb://localhost/studentsDb",{useNewUrlParser: true, useUni
         process.exit();
       });
 
+//This connection responds to my local machine studentsDb
+/*mongoose.connect("mongodb://localhost/studentsDb",{useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    console.log('Connected to the Database');
+})
+.catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+      });
+*/
       
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
